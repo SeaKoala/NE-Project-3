3d739622578c72a448f62f940ffe96703c8a71ea
Alec Mehra <43073945+SeaKoala@users.noreply.github.com>
2021-04-21 23:06:31 -0500

    fixed a lot, much better plots

--- NO_PLOTS_ALLOWED.m
+++ NO_PLOTS_ALLOWED.m
@@ -137,143 +137,162 @@ sub2POST_EEG = BPF(sub2POST_EEG);
 %sub2POST_EEG = abs(sub2POST_EEG);
 
 %% Build dataset by Task
+% 
+% totalTrials = 75;
+% 
+% extStartIndex_PRE = find(sub1PRE_TYP == 100);
+% extStopIndex_PRE = find(sub1PRE_TYP == 102);
+% flxStartIndex_PRE = find(sub1PRE_TYP == 300);
+% flxStopIndex_PRE = find(sub1PRE_TYP == 302);
+% restStartIndex_PRE = find(sub1PRE_TYP == 400);
+% restStopIndex_PRE = find(sub1PRE_TYP == 402);
+% 
+% startIndexs = [extStartIndex_PRE; flxStartIndex_PRE; restStartIndex_PRE];
+% stopIndexs = [extStopIndex_PRE; flxStopIndex_PRE; restStopIndex_PRE];
+% 
+% sizes = zeros(25,1);
+% 
+% for i = 1:75
+%     sizes(i) = size(sub1PRE_EEG(sub1PRE_POS(startIndexs(i))-1024:sub1PRE_POS(stopIndexs(i)), :), 1);
+% end
+% max_samples = (max(sizes))
+% 
+% sub1PRE_DATA = zeros(max_samples, 32,totalTrials);
+% 
+% for i = 1:75
+%     sub1PRE_DATA(1:sizes(i),:,i) = sub1PRE_EEG(sub1PRE_POS(startIndexs(i))-1024:sub1PRE_POS(stopIndexs(i)), :); 
+% end
+% 
+% 
+% extStartIndex_POST = find(sub1POST_TYP == 101);
+% extStopIndex_POST = find(sub1POST_TYP == 102);
+% flxStartIndex_POST = find(sub1POST_TYP == 301);
+% flxStopIndex_POST = find(sub1POST_TYP == 302);
+% restStartIndex_POST = find(sub1POST_TYP == 401);
+% restStopIndex_POST = find(sub1POST_TYP == 402);
+% 
+% startIndexs = [extStartIndex_POST; flxStartIndex_POST; restStartIndex_POST];
+% stopIndexs = [extStopIndex_POST; flxStopIndex_POST; restStopIndex_POST];
+% 
+% sizes = zeros(25,1);
+% 
+% for i = 1:75
+%     sizes(i) = size(sub1POST_EEG(sub1POST_POS(startIndexs(i)):sub1POST_POS(stopIndexs(i)), :), 1);
+% end
+% max_samples = (max(sizes));
+% 
+% sub1POST_DATA = zeros(max_samples, 32,totalTrials);
+% 
+% for i = 1:75
+%     sub1POST_DATA(1:sizes(i),:,i) = sub1POST_EEG(sub1POST_POS(startIndexs(i)):sub1POST_POS(stopIndexs(i)), :); 
+% end
+% 
+% extStartIndex_PRE = find(sub2PRE_TYP == 101);
+% extStopIndex_PRE = find(sub2PRE_TYP == 102);
+% flxStartIndex_PRE = find(sub2PRE_TYP == 301);
+% flxStopIndex_PRE = find(sub2PRE_TYP == 302);
+% restStartIndex_PRE = find(sub2PRE_TYP == 401);
+% restStopIndex_PRE = find(sub2PRE_TYP == 402);
+% 
+% startIndexs = [extStartIndex_PRE; flxStartIndex_PRE; restStartIndex_PRE];
+% stopIndexs = [extStopIndex_PRE; flxStopIndex_PRE; restStopIndex_PRE];
+% 
+% sizes = zeros(25,1);
+% 
+% for i = 1:75
+%     sizes(i) = size(sub2PRE_EEG(sub2PRE_POS(startIndexs(i)):sub2PRE_POS(stopIndexs(i)), :), 1);
+% end
+% max_samples = (max(sizes));
+% 
+% sub2PRE_DATA = zeros(max_samples, 32,totalTrials);
+% 
+% for i = 1:75
+%     sub2PRE_DATA(1:sizes(i),:,i) = sub2PRE_EEG(sub2PRE_POS(startIndexs(i)):sub2PRE_POS(stopIndexs(i)), :); 
+% end
+% 
+% 
+% extStartIndex_POST = find(sub2POST_TYP == 101);
+% extStopIndex_POST = find(sub2POST_TYP == 102);
+% flxStartIndex_POST = find(sub2POST_TYP == 301);
+% flxStopIndex_POST = find(sub2POST_TYP == 302);
+% restStartIndex_POST = find(sub2POST_TYP == 401);
+% restStopIndex_POST = find(sub2POST_TYP == 402);
+% 
+% startIndexs = [extStartIndex_POST; flxStartIndex_POST; restStartIndex_POST];
+% stopIndexs = [extStopIndex_POST; flxStopIndex_POST; restStopIndex_POST];
+% 
+% sizes = zeros(25,1);
+% 
+% for i = 1:75
+%     sizes(i) = size(sub2POST_EEG(sub2POST_POS(startIndexs(i)):sub2POST_POS(stopIndexs(i)), :), 1);
+% end
+% max_samples = (max(sizes));
+% 
+% sub2POST_DATA = zeros(max_samples, 32,totalTrials);
+% 
+% for i = 1:75
+%     sub2POST_DATA(1:sizes(i),:,i) = sub2POST_EEG(sub2POST_POS(startIndexs(i)):sub2POST_POS(stopIndexs(i)), :); 
+% end
 
-totalTrials = 75;
-
-extStartIndex_PRE = find(sub1PRE_TYP == 101);
-extStopIndex_PRE = find(sub1PRE_TYP == 102);
-flxStartIndex_PRE = find(sub1PRE_TYP == 301);
-flxStopIndex_PRE = find(sub1PRE_TYP == 302);
-restStartIndex_PRE = find(sub1PRE_TYP == 401);
-restStopIndex_PRE = find(sub1PRE_TYP == 402);
-
-startIndexs = [extStartIndex_PRE; flxStartIndex_PRE; restStartIndex_PRE];
-stopIndexs = [extStopIndex_PRE; flxStopIndex_PRE; restStopIndex_PRE];
-
-sizes = zeros(25,1);
-
-for i = 1:75
-    sizes(i) = size(sub1PRE_EEG(sub1PRE_POS(startIndexs(i)):sub1PRE_POS(stopIndexs(i)), :), 1);
-end
-max_samples = (max(sizes));
-
-sub1PRE_DATA = zeros(max_samples, 32,totalTrials);
-
-for i = 1:75
-    sub1PRE_DATA(1:sizes(i),:,i) = sub1PRE_EEG(sub1PRE_POS(startIndexs(i)):sub1PRE_POS(stopIndexs(i)), :); 
-end
 
 
-extStartIndex_POST = find(sub1POST_TYP == 101);
-extStopIndex_POST = find(sub1POST_TYP == 102);
-flxStartIndex_POST = find(sub1POST_TYP == 301);
-flxStopIndex_POST = find(sub1POST_TYP == 302);
-restStartIndex_POST = find(sub1POST_TYP == 401);
-restStopIndex_POST = find(sub1POST_TYP == 402);
-
-startIndexs = [extStartIndex_POST; flxStartIndex_POST; restStartIndex_POST];
-stopIndexs = [extStopIndex_POST; flxStopIndex_POST; restStopIndex_POST];
-
-sizes = zeros(25,1);
-
-for i = 1:75
-    sizes(i) = size(sub1POST_EEG(sub1POST_POS(startIndexs(i)):sub1POST_POS(stopIndexs(i)), :), 1);
-end
-max_samples = (max(sizes));
-
-sub1POST_DATA = zeros(max_samples, 32,totalTrials);
-
-for i = 1:75
-    sub1POST_DATA(1:sizes(i),:,i) = sub1POST_EEG(sub1POST_POS(startIndexs(i)):sub1POST_POS(stopIndexs(i)), :); 
-end
-
-extStartIndex_PRE = find(sub2PRE_TYP == 101);
-extStopIndex_PRE = find(sub2PRE_TYP == 102);
-flxStartIndex_PRE = find(sub2PRE_TYP == 301);
-flxStopIndex_PRE = find(sub2PRE_TYP == 302);
-restStartIndex_PRE = find(sub2PRE_TYP == 401);
-restStopIndex_PRE = find(sub2PRE_TYP == 402);
-
-startIndexs = [extStartIndex_PRE; flxStartIndex_PRE; restStartIndex_PRE];
-stopIndexs = [extStopIndex_PRE; flxStopIndex_PRE; restStopIndex_PRE];
-
-sizes = zeros(25,1);
-
-for i = 1:75
-    sizes(i) = size(sub2PRE_EEG(sub2PRE_POS(startIndexs(i)):sub2PRE_POS(stopIndexs(i)), :), 1);
-end
-max_samples = (max(sizes));
-
-sub2PRE_DATA = zeros(max_samples, 32,totalTrials);
-
-for i = 1:75
-    sub2PRE_DATA(1:sizes(i),:,i) = sub2PRE_EEG(sub2PRE_POS(startIndexs(i)):sub2PRE_POS(stopIndexs(i)), :); 
-end
-
-
-extStartIndex_POST = find(sub2POST_TYP == 101);
-extStopIndex_POST = find(sub2POST_TYP == 102);
-flxStartIndex_POST = find(sub2POST_TYP == 301);
-flxStopIndex_POST = find(sub2POST_TYP == 302);
-restStartIndex_POST = find(sub2POST_TYP == 401);
-restStopIndex_POST = find(sub2POST_TYP == 402);
-
-startIndexs = [extStartIndex_POST; flxStartIndex_POST; restStartIndex_POST];
-stopIndexs = [extStopIndex_POST; flxStopIndex_POST; restStopIndex_POST];
-
-sizes = zeros(25,1);
-
-for i = 1:75
-    sizes(i) = size(sub2POST_EEG(sub2POST_POS(startIndexs(i)):sub2POST_POS(stopIndexs(i)), :), 1);
-end
-max_samples = (max(sizes));
-
-sub2POST_DATA = zeros(max_samples, 32,totalTrials);
-
-for i = 1:75
-    sub2POST_DATA(1:sizes(i),:,i) = sub2POST_EEG(sub2POST_POS(startIndexs(i)):sub2POST_POS(stopIndexs(i)), :); 
-end
+%%
+sub1PRE_DATA = slice_n_dice(sub1PRE_EEG, sub1PRE_TYP, sub1PRE_POS); 
+sub1POST_DATA = slice_n_dice(sub1POST_EEG, sub1POST_TYP, sub1POST_POS); 
+sub2PRE_DATA = slice_n_dice(sub2PRE_EEG, sub2PRE_TYP, sub2PRE_POS); 
+sub2POST_DATA = slice_n_dice(sub2POST_EEG, sub2POST_TYP, sub2POST_POS); 
 
 taskType  = [{'EXT', 'FLX', 'REST'}];
 results(1:25) = taskType(1);
 results(26:50) = taskType(2);
 results(51:75) = taskType(3);
 
-
 %% Grand Average
-GAVG_1_PRE = zeros(size(sub1PRE_DATA, 1), 32, 3);
-GAVG_1_POST = zeros(size(sub1POST_DATA, 1), 32, 3);
-GAVG_2_PRE = zeros(size(sub2PRE_DATA, 1), 32, 3);
-GAVG_2_POST = zeros(size(sub2POST_DATA, 1), 32, 3);
+% GAVG_1_PRE = zeros(size(sub1PRE_DATA, 1), 32, 3);
+% GAVG_1_POST = zeros(size(sub1POST_DATA, 1), 32, 3);
+% GAVG_2_PRE = zeros(size(sub2PRE_DATA, 1), 32, 3);
+% GAVG_2_POST = zeros(size(sub2POST_DATA, 1), 32, 3);
+% 
+% for i = 1:size(sub1PRE_DATA, 1)
+%     for t =1:25
+%         GAVG_1_PRE(i,:, 1) = sub1PRE_DATA(i, :, t);
+%         GAVG_1_PRE(i,:, 2) = sub1PRE_DATA(i, :, t+25);
+%         GAVG_1_PRE(i,:, 3) = sub1PRE_DATA(i, :, t+50);
+%     end
+% end
+% for i = 1:size(sub1POST_DATA, 1)
+%     for t =1:25
+%         GAVG_1_POST(i,:, 1) = sub1POST_DATA(i, :, t);
+%         GAVG_1_POST(i,:, 2) = sub1POST_DATA(i, :, t+25);
+%         GAVG_1_POST(i,:, 3) = sub1POST_DATA(i, :, t+50);
+%     end
+% end
+% for i = 1:size(sub2PRE_DATA, 1)
+%     for t =1:25
+%         GAVG_2_PRE(i,:, 1) = sub2PRE_DATA(i, :, t);
+%         GAVG_2_PRE(i,:, 2) = sub2PRE_DATA(i, :, t+25);
+%         GAVG_2_PRE(i,:, 3) = sub2PRE_DATA(i, :, t+50);
+%     end
+% end
+% for i = 1:size(sub2POST_DATA, 1)
+%     for t =1:25
+%         GAVG_2_POST(i,:, 1) = sub2POST_DATA(i, :, t);
+%         GAVG_2_POST(i,:, 2) = sub2POST_DATA(i, :, t+25);
+%         GAVG_2_POST(i,:, 3) = sub2POST_DATA(i, :, t+50);
+%     end
+% end
+GAVG_1_PRE = Gavg(sub1PRE_DATA);
+GAVG_1_POST = Gavg(sub1POST_DATA);
+GAVG_2_PRE = Gavg(sub2PRE_DATA);
+GAVG_2_POST = Gavg(sub2POST_DATA);
+%%
+wsize = 1;
 
-for i = 1:size(sub1PRE_DATA, 1)
-    for t =1:25
-        GAVG_1_PRE(i,:, 1) = sub1PRE_DATA(i, :, t);
-        GAVG_1_PRE(i,:, 2) = sub1PRE_DATA(i, :, t+25);
-        GAVG_1_PRE(i,:, 3) = sub1PRE_DATA(i, :, t+50);
-    end
-end
-for i = 1:size(sub1POST_DATA, 1)
-    for t =1:25
-        GAVG_1_POST(i,:, 1) = sub1POST_DATA(i, :, t);
-        GAVG_1_POST(i,:, 2) = sub1POST_DATA(i, :, t+25);
-        GAVG_1_POST(i,:, 3) = sub1POST_DATA(i, :, t+50);
-    end
-end
-for i = 1:size(sub2PRE_DATA, 1)
-    for t =1:25
-        GAVG_2_PRE(i,:, 1) = sub2PRE_DATA(i, :, t);
-        GAVG_2_PRE(i,:, 2) = sub2PRE_DATA(i, :, t+25);
-        GAVG_2_PRE(i,:, 3) = sub2PRE_DATA(i, :, t+50);
-    end
-end
-for i = 1:size(sub2POST_DATA, 1)
-    for t =1:25
-        GAVG_2_POST(i,:, 1) = sub2POST_DATA(i, :, t);
-        GAVG_2_POST(i,:, 2) = sub2POST_DATA(i, :, t+25);
-        GAVG_2_POST(i,:, 3) = sub2POST_DATA(i, :, t+50);
-    end
-end
+[Pavg_1PRE] = segmentation(GAVG_1_PRE, wsize, 0, 1, 'alpha', channels);
+[Pavg_1POST] = segmentation(GAVG_1_POST, wsize, 0);
+[Pavg_2PRE] = segmentation(GAVG_2_PRE, wsize, 0);
+[Pavg_2POST] = segmentation(GAVG_2_POST, wsize, 0);
+
 
 %% Feature Extraction
 [var_1PRE, kurt_1PRE, skew_1PRE, mse_1PRE, mae_1PRE, SNR_1PRE, PSNR_1PRE,  Pavg_1PRE]  = feat_extract(sub1PRE_DATA);
@@ -359,4 +378,71 @@ function [var, kurt, skew, mse, mae, SNR, PSNR, Pavg]  = feat_extract(data)
             end
         end
     end
+end
+
+
+function [x_DATA] = slice_n_dice(eeg, typ, pos) 
+
+    totalTrials = 75;
+
+    extStartIndex_PRE = find(typ == 100);
+    extStopIndex_PRE = find(typ == 102);
+    flxStartIndex_PRE = find(typ == 300);
+    flxStopIndex_PRE = find(typ == 302);
+    restStartIndex_PRE = find(typ == 400);
+    restStopIndex_PRE = find(typ == 402);
+
+    startIndexs = [extStartIndex_PRE; flxStartIndex_PRE; restStartIndex_PRE];
+    stopIndexs = [extStopIndex_PRE; flxStopIndex_PRE; restStopIndex_PRE];
+
+    sizes = zeros(25,1);
+
+    for i = 1:75
+        sizes(i) = size(eeg(pos(startIndexs(i))-1024:pos(stopIndexs(i)), :), 1);
+    end
+    max_samples = (max(sizes))
+
+    x_DATA = zeros(max_samples, 32,totalTrials);
+
+    for i = 1:75
+        x_DATA(1:sizes(i),:,i) = eeg(pos(startIndexs(i))-1024:pos(stopIndexs(i)), :); 
+    end
+end
+
+function [Pavg] = segmentation(data, Wsize, Olap, task, name, channels);
+    
+    fs = 512;
+    WSize = floor(Wsize*fs);	    % length of each data frame, 30ms
+    nOlap = floor(Olap*WSize);  % overlap of successive frames, half of WSize
+    hop = WSize-nOlap;	    % amount to advance for next data frame
+    nx = length(data(:,1,1));            % length of input vector
+    len = fix((nx - (WSize-hop))/hop);	%length of output vector = total frames
+    
+%     [MAV, WT] = deal(zeros(tasks, len, 4));
+%     [MAV_max, WT_max] = deal(zeros(tasks, 4));
+    Pavg = zeros(len, 32, 3);
+        
+    for T = 1:3
+        for c = 1:32
+            for i = 1:len
+                segment = data(         (((i-1)*hop+1):((i-1)*hop+WSize)),  c,          T);
+                Pavg(i, c, T) = 1/length(segment) * sum(      segment(1:length(segment)).^2          );
+    %             MAV(t, i, p) = mean(abs(segment));
+    %             WT(t, i, p) = jfemg('ewl', segment);
+            end
+        end
+    end
+end
+
+function gavg = Gavg(x)
+    gavg = zeros(size(x, 1), 32, 3);
+
+    for i = 1:size(x, 1)
+        for t =1:25
+            gavg(i,:, 1) = gavg(i,:, 1) + x(i, :, t);
+            gavg(i,:, 2) = gavg(i,:, 2) +  x(i, :, t+25);
+            gavg(i,:, 3) = gavg(i,:, 3)+ x(i, :, t+50);
+        end
+    end
+        gavg = gavg(:,:, :)./25;
 end
--- Plots.m
+++ Plots.m
@@ -15,3 +15,77 @@ for i = 1:5
     subplot(2,5,i+5)
     plot_topography(channels, Pavg_1POST(:,i+25));
 end
+
+%%
+figure
+hold on
+plot(sub1PRE_DATA(:,1,25))
+plot(GAVG_1_PRE(:,1,1))
+plot(gavg(:,1,1))
+hold off
+legend
+
+%%
+task = 1;
+
+channels(13) = {'T9'};
+channels(19) = {'T10'};
+figure
+sgtitle('G AVG Power Sub 1 PRE TESS EXT');
+
+% plot_topography.make_contour
+% for i = 1:2
+max = max(max(Pavg_1PRE(:,:, task)));
+min =  min(min(Pavg_1PRE(:,:, task)));
+% norm = max(max(Pavg_1PRE(:,:, 2))) - min(min(Pavg_1PRE(:,:, 2)));
+
+for i = 1:size(Pavg_1PRE,1)
+%     Pavg_1PRE(i,13, task) = max;
+%     Pavg_1PRE(i,19, task) = min;
+    subplot(4,2/wsize,i)
+    hold on
+    str = ['TIME: ' , num2str((i-1)*wsize) , ' :: ' , num2str((i)*wsize)];
+    title(str)
+    plot_topography(channels, Pavg_1PRE(i,:, task), true);
+    hold off
+end
+
+%%
+plotz(Pavg_1PRE, 1, 2, '1 PRE' , channels)
+% plotz(Pavg_1POST, 1, 2, '1 POST' , channels)
+% plotz(Pavg_2PRE, 1, 2, '2 PRE' , channels)
+% plotz(Pavg_2POST, 1, 2, '2 POST' , channels)
+
+%%
+
+function plotz(Pavg, Wsize, task, name, channels)
+    taskType  = [{'EXT', 'FLX', 'REST'}];
+%     timePeriod = [{'Fixation' , 'Task Cue', 'Tast Exct'}];
+
+        channels(13) = {'T9'};
+        channels(19) = {'T10'};
+        figure
+        sgtitle(['AVG Power ' , name,  taskType(task)]);
+%         labCount =1;
+        % plot_topography.make_contour
+        % for i = 1:2
+%         max = max(max(Pavg_1PRE(:,:, task)));
+%         min =  min(min(Pavg_1PRE(:,:, task)));
+        % norm = max(max(Pavg_1PRE(:,:, 2))) - min(min(Pavg_1PRE(:,:, 2)));
+
+        for i = 1:size(Pavg,1)
+        %     Pavg_1PRE(i,13, task) = max;
+        %     Pavg_1PRE(i,19, task) = min;
+            subplot(4,2/Wsize,i)
+            hold on
+%             if (mod(i-1, 2/Wsize) == 0)
+%                 ylabel(timePeriod(labCount))
+%                 labCount = labCount +1;
+%             end
+            str = ['TIME: ' , num2str((i-1)*Wsize) , ' :: ' , num2str((i)*Wsize)];
+            title(str)
+%             ylabel('Fix');
+            plot_topography(channels, Pavg(i,:, task), true);
+            hold off
+        end
+end
